d94190f3a9f69e57c4764350df0e16c4e46e5d90
Shaniii1994 <shaniiisiddiqui94@gmail.com>
2025-01-08 23:27:37 +0500

    Update Projects.tsx

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -8,14 +8,14 @@ const data = [
     id: 0,
     title: 'Responsive Website',
     descr: 'A multi-page responsive Next.js website using React components with TailwindCSS.',
-    img: '/WhatsApp Image 2024-11-09 at 11.46.42 AM.png',
+    img: '/WhatsApp Image 2024-11-09 at 11.46.42 AM.jpg',
     tags: ["React", "Next.js", 'Node', 'TypeScript', 'TailwindCSS'],
   },
   {
     id: 1,
     title: 'Static Interactive Resume',
     descr: 'A Static Resume Builder using HTML, TypeScript, and TailwindCSS.',
-    img: '/pic-2.png',
+    img: '/pic-2.jpg',
     tags: ['HTML', 'Node', 'TypeScript', 'CSS'],
   },
 ];
@@ -27,7 +27,7 @@ const Projects = () => {
       <div className="grid gap-10 xl:gap-0 xl:gap-y-10 md:grid-cols-2 lg:grid-cols-3 place-items-center">
         {data.map((el) => (<Card 
         key={el.id}
-        title={el.title}
+        title={el.Projects}
         desc={el.descr}
         img={el.img}
         tags={el.tags}  
