6dc0f4b0a1a3a1cbd5c6d00a8b0768845034b7c2
Shaniii1994 <shaniiisiddiqui94@gmail.com>
2025-01-08 23:46:29 +0500

    Update Heading.tsx

--- src/app/components/Heading.tsx
+++ src/app/components/Heading.tsx
@@ -8,7 +8,7 @@ const Heading: React.FC<PropsType> = ({ title }) => {
   return (
     <div className='text-center text-4xl pb-8'>
       <p className='border-b-4 border-yellow-400 inline-block pb-2 text-4xl text-orange-600 hover:text-yellow-500'>
-        {"title"}
+        {"Projects"}
       </p>
     </div>
   );
